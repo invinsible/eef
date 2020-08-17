f986b557cf45c9dbc698e903bc38ea56cde12df3
Sergey Butyanov <igor@tomatstudio.ru>
2020-08-17 16:58:49 +0300

    Blur background in menu

--- js/test.js
+++ js/test.js
@@ -1,8 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu-wrapper');
 
 menuBtn.addEventListener('click', function(e){
     e.preventDefault();
     this.classList.toggle('active');
-    menu.classList.toggle('active');
+    document.body.classList.toggle('show-menu');
 });
