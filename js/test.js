6fb049bfa3b6d57d1acdd000d55588a93a65f39f
Sergey Butyanov <igor@tomatstudio.ru>
2020-08-19 16:49:24 +0300

    Menu adaptive

--- js/test.js
+++ js/test.js
@@ -1,7 +1,25 @@
-const menuBtn = document.querySelector('.menu-btn');
+// const menuBtn = document.querySelector('.menu-btn');
 
-menuBtn.addEventListener('click', function(e){
+// menuBtn.addEventListener('click', function(e){
+//     e.preventDefault();
+//     this.classList.toggle('active');
+//     document.body.classList.toggle('show-menu');
+// });
+
+$('.menu-btn').click(function(e){
     e.preventDefault();
-    this.classList.toggle('active');
-    document.body.classList.toggle('show-menu');
+    $(this).toggleClass('active');
+    $(document.body).toggleClass('show-menu');
+});
+
+
+$('.hasSub').click(function(e){
+    e.preventDefault();
+    $(this).next().toggleClass('show-sub');
+    $('.menu__link').addClass('hidden');
+})
+
+$('.menu__back').click(function(){
+    $(this).parent().removeClass('show-sub');
+    $('.menu__link').removeClass('hidden');
 });
